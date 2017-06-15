c0d980d102467243a52a0ab8719677e164e34d42
Arief Wijaya <hoaaah.arief@gmail.com>
2017-06-15 10:22:22 +0700

    .

---
+++ src/Breadcrumb.php
@@ -0,0 +1,13 @@
+<?php
+namespace hoaaah\LaravelBreadcrumb;
+
+class Breadcrumb {
+    public static function{
+        return '
+            <ul class="breadcrumb">
+                <li><a href="/home">Home</a></li>
+                <li class="active">Link Okeh</li>
+            </ul>
+        ';
+    }
+}
